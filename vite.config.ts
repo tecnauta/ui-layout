d44ea3db5ee51ed1dbd01aa19298163081547ee8
danieloprado <danielprado.ad@gmail.com>
2023-06-30 15:06:16 -0300

    feat: sourcemap

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,11 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 import dts from 'vite-plugin-dts';
-
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: '.',
-    cssCodeSplit: true,
+    sourcemap: true,
     lib: {
       entry: './index.tsx',
       name: '@eduzz/ui-layout',
@@ -25,12 +24,5 @@ export default defineConfig({
       }
     }
   },
-  plugins: [
-    react(),
-    cssInjectedByJsPlugin({
-      topExecutionPriority: true,
-      relativeCSSInjection: true
-    }),
-    dts({ copyDtsFiles: true })
-  ]
+  plugins: [react(), cssInjectedByJsPlugin({ topExecutionPriority: true }), dts({ copyDtsFiles: true })]
 });
