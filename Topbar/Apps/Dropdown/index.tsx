91ac201a1fff2f0f5b48a4be1f9f5c7a26d0a05d
danieloprado <danielprado.ad@gmail.com>
2023-06-30 13:13:43 -0300

    faet: tailwind prefix

--- Topbar/Apps/Dropdown/index.tsx
+++ Topbar/Apps/Dropdown/index.tsx
@@ -34,10 +34,10 @@ const AppsDropdown = memo<AppsDropdownProps>(({ currentApplication, applications
   return (
     <div
       className={cx(
-        'group/apps top-[var(--eduzz-ui-layout-topbar-height-rem)] z-[105] max-h-[calc(100vh-var(--eduzz-ui-layout-topbar-height-rem))] rounded-[0_0_0.5rem_0.5rem] shadow-[0px_8px_24px_rgba(0,0,0,0.16)] sm:w-[var(--eduzz-ui-layout-topbar-app-dropdown-width-rem)] eduzz-ui-tw-invisible eduzz-ui-tw-fixed eduzz-ui-tw-left-0 eduzz-ui-tw-box-border eduzz-ui-tw-w-full eduzz-ui-tw-select-none eduzz-ui-tw-overflow-y-auto eduzz-ui-tw-bg-white sm:eduzz-ui-tw-left-2',
+        'eduzz-ui-tw-group/apps eduzz-ui-tw-invisible eduzz-ui-tw-fixed eduzz-ui-tw-left-0 eduzz-ui-tw-top-[var(--eduzz-ui-layout-topbar-height-rem)] eduzz-ui-tw-z-[105] eduzz-ui-tw-box-border eduzz-ui-tw-max-h-[calc(100vh-var(--eduzz-ui-layout-topbar-height-rem))] eduzz-ui-tw-w-full eduzz-ui-tw-select-none eduzz-ui-tw-overflow-y-auto eduzz-ui-tw-rounded-[0_0_0.5rem_0.5rem] eduzz-ui-tw-bg-white eduzz-ui-tw-shadow-[0px_8px_24px_rgba(0,0,0,0.16)] sm:eduzz-ui-tw-left-2 sm:eduzz-ui-tw-w-[var(--eduzz-ui-layout-topbar-app-dropdown-width-rem)]',
         {
-          '--opened !visible': opened,
-          '--expanded !left-0 h-[calc(100%-var(--eduzz-ui-layout-topbar-height-rem))] !w-full eduzz-ui-tw-overflow-x-auto eduzz-ui-tw-rounded-none eduzz-ui-tw-shadow-none':
+          '--opened !eduzz-ui-tw-visible': opened,
+          '--expanded !eduzz-ui-tw-left-0 eduzz-ui-tw-h-[calc(100%-var(--eduzz-ui-layout-topbar-height-rem))] !eduzz-ui-tw-w-full eduzz-ui-tw-overflow-x-auto eduzz-ui-tw-rounded-none eduzz-ui-tw-shadow-none':
             expanded
         }
       )}
@@ -50,7 +50,7 @@ const AppsDropdown = memo<AppsDropdownProps>(({ currentApplication, applications
         </button>
       </div>
 
-      <div className='grid-cols-[repeat(2,1fr)] gap-[0.5rem_0.5rem] group-[.--expanded]/apps:grid-cols-[1fr] sm:grid-cols-[repeat(3,1fr)] sm:group-[.--expanded]/apps:grid-cols-[repeat(2,1fr)] lg:group-[.--expanded]/apps:grid-cols-[repeat(3,1fr)] xl:group-[.--expanded]/apps:grid-cols-[repeat(4,1fr)] eduzz-ui-tw-box-border eduzz-ui-tw-grid eduzz-ui-tw-flex-wrap eduzz-ui-tw-justify-items-center eduzz-ui-tw-p-4 group-[.--expanded]/apps:eduzz-ui-tw-gap-0 group-[.--expanded]/apps:eduzz-ui-tw-p-0 sm:group-[.--expanded]/apps:eduzz-ui-tw-gap-6 sm:group-[.--expanded]/apps:eduzz-ui-tw-p-6'>
+      <div className='eduzz-ui-tw-box-border eduzz-ui-tw-grid eduzz-ui-tw-grid-cols-[repeat(2,1fr)] eduzz-ui-tw-flex-wrap eduzz-ui-tw-justify-items-center eduzz-ui-tw-gap-[0.5rem_0.5rem] eduzz-ui-tw-p-4 group-[.--expanded]/apps:eduzz-ui-tw-grid-cols-[1fr] group-[.--expanded]/apps:eduzz-ui-tw-gap-0 group-[.--expanded]/apps:eduzz-ui-tw-p-0 sm:eduzz-ui-tw-grid-cols-[repeat(3,1fr)] sm:group-[.--expanded]/apps:eduzz-ui-tw-grid-cols-[repeat(2,1fr)] sm:group-[.--expanded]/apps:eduzz-ui-tw-gap-6 sm:group-[.--expanded]/apps:eduzz-ui-tw-p-6 lg:group-[.--expanded]/apps:eduzz-ui-tw-grid-cols-[repeat(3,1fr)] xl:group-[.--expanded]/apps:eduzz-ui-tw-grid-cols-[repeat(4,1fr)]'>
         {!applications?.length && (
           <div className='eduzz-ui-tw-col-start-1 eduzz-ui-tw-col-end-4 eduzz-ui-tw-flex eduzz-ui-tw-w-full eduzz-ui-tw-items-center eduzz-ui-tw-justify-center eduzz-ui-tw-p-12'>
             Carregando...
@@ -63,7 +63,7 @@ const AppsDropdown = memo<AppsDropdownProps>(({ currentApplication, applications
           return (
             <a
               className={cx(
-                'grid-cols-[2rem_1fr] grid-rows-[1.5rem_auto] hover:bg-[rgba(0,0,0,0.04)] sm:grid-cols-[4rem_1fr] eduzz-ui-tw-box-border eduzz-ui-tw-block eduzz-ui-tw-w-full eduzz-ui-tw-cursor-pointer eduzz-ui-tw-gap-1 eduzz-ui-tw-rounded eduzz-ui-tw-border-gray-200 eduzz-ui-tw-px-2 eduzz-ui-tw-py-4 eduzz-ui-tw-no-underline group-[.--expanded]/apps:eduzz-ui-tw-grid group-[.--expanded]/apps:eduzz-ui-tw-border-b group-[.--expanded]/apps:eduzz-ui-tw-p-4 sm:group-[.--expanded]/apps:eduzz-ui-tw-border',
+                'eduzz-ui-tw-box-border eduzz-ui-tw-block eduzz-ui-tw-w-full eduzz-ui-tw-cursor-pointer eduzz-ui-tw-grid-cols-[2rem_1fr] eduzz-ui-tw-grid-rows-[1.5rem_auto] eduzz-ui-tw-gap-1 eduzz-ui-tw-rounded eduzz-ui-tw-border-gray-200 eduzz-ui-tw-px-2 eduzz-ui-tw-py-4 eduzz-ui-tw-no-underline hover:eduzz-ui-tw-bg-[rgba(0,0,0,0.04)] group-[.--expanded]/apps:eduzz-ui-tw-grid group-[.--expanded]/apps:eduzz-ui-tw-border-b group-[.--expanded]/apps:eduzz-ui-tw-p-4 sm:eduzz-ui-tw-grid-cols-[4rem_1fr] sm:group-[.--expanded]/apps:eduzz-ui-tw-border',
                 isCurrent && 'bg-[rgba(0,0,0,0.04)]'
               )}
               key={app.application}
@@ -74,7 +74,7 @@ const AppsDropdown = memo<AppsDropdownProps>(({ currentApplication, applications
             >
               <img
                 src={app.icon}
-                className='eduzz-ui-tw-group-[.--expanded]/apps:max-h-[1.50rem] group-[.--expanded]/apps:max-w-[1.50rem] sm:group-[.--expanded]/apps:max-h-[3.1rem] sm:group-[.--expanded]/apps:max-w-[3.1rem] eduzz-ui-tw-m-auto eduzz-ui-tw-mb-2 eduzz-ui-tw-max-h-10 eduzz-ui-tw-max-w-[2.5rem]'
+                className='eduzz-ui-tw-m-auto eduzz-ui-tw-mb-2 eduzz-ui-tw-max-h-10 eduzz-ui-tw-max-w-[2.5rem] group-[.--expanded]/apps:eduzz-ui-tw-max-h-[1.50rem] group-[.--expanded]/apps:eduzz-ui-tw-max-w-[1.50rem] sm:group-[.--expanded]/apps:eduzz-ui-tw-max-h-[3.1rem] sm:group-[.--expanded]/apps:eduzz-ui-tw-max-w-[3.1rem]'
               />
 
               <p className='eduzz-ui-tw-text-center eduzz-ui-tw-text-sm group-[.--expanded]/apps:eduzz-ui-tw-text-left group-[.--expanded]/apps:eduzz-ui-tw-text-base'>
@@ -90,7 +90,7 @@ const AppsDropdown = memo<AppsDropdownProps>(({ currentApplication, applications
 
       {!!applications?.length && (
         <button
-          className='eduzz-ui-tw-hover:bg-[rgba(0,0,0,0.04)] eduzz-ui-tw-flex eduzz-ui-tw-h-[50px] eduzz-ui-tw-w-full eduzz-ui-tw-flex-row eduzz-ui-tw-items-center eduzz-ui-tw-justify-center eduzz-ui-tw-gap-2 group-[.--expanded]/apps:eduzz-ui-tw-hidden'
+          className='eduzz-ui-tw-flex eduzz-ui-tw-h-[50px] eduzz-ui-tw-w-full eduzz-ui-tw-flex-row eduzz-ui-tw-items-center eduzz-ui-tw-justify-center eduzz-ui-tw-gap-2 hover:eduzz-ui-tw-bg-[rgba(0,0,0,0.04)] group-[.--expanded]/apps:eduzz-ui-tw-hidden'
           onClick={toggleExpanded}
         >
           <IconFullscreen />
