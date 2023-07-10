91803d9411f643abb1e8b50e2623935ea438e3c0
danieloprado <danielprado.ad@gmail.com>
2023-07-10 15:43:42 -0300

    feat: update

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,13 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 import dts from 'vite-plugin-dts';
+
+import packageJson from './package.json';
+
+const external = ['react/jsx-runtime']
+  .concat(Object.keys(packageJson.dependencies))
+  .concat(Object.keys(packageJson.devDependencies));
+
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
@@ -14,13 +21,14 @@ export default defineConfig({
       fileName: format => `ui-layout.${format}.js`
     },
     rollupOptions: {
-      external: [/node_modules/],
+      external,
       output: {
         preserveModules: true,
         preserveModulesRoot: '.',
         inlineDynamicImports: false,
         entryFileNames: ({ name: fileName }) => `${fileName}.js`,
-        exports: 'named'
+        exports: 'named',
+        globals: external.reduce((acc, e) => ({ ...acc, [e]: e }), {})
       }
     }
   },
