982b071e72bda91698144d9a3ac6216bdfdfd36a
danieloprado <danielprado.ad@gmail.com>
2023-09-19 11:15:59 -0300

    fix: margin of usermenu group

--- Topbar/UserMenu/ItemGroup/index.tsx
+++ Topbar/UserMenu/ItemGroup/index.tsx
@@ -6,7 +6,7 @@ export type UserMenuGroupProps = React.HTMLAttributes<HTMLDivElement> & {
 const UserMenuGroup = ({ label, children, ...rest }: UserMenuGroupProps) => {
   return (
     <div className='uizz-layout-flex uizz-layout-flex-col' {...rest}>
-      <p className='uizz-layout-mt-2 uizz-layout-px-4 uizz-layout-py-2 uizz-layout-text-sm uizz-layout-font-bold uizz-layout-tracking-[0.3px]'>
+      <p className='uizz-layout-mb-0 uizz-layout-mt-2 uizz-layout-px-4 uizz-layout-py-2 uizz-layout-text-sm uizz-layout-font-bold uizz-layout-tracking-[0.3px]'>
         {label}
       </p>
       {children}
